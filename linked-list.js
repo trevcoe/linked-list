f219bb890f0830add4bea9f8ae1a7047bde7055c
trevcoe <88930059+trevcoe@users.noreply.github.com>
2022-03-22 00:00:12 -0400

    Add files via upload

---
+++ linked-list.js
@@ -0,0 +1,137 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+
+  /** push(val): add new value to end of list. */
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+  }
+
+  /** unshift(val): add new value to start of list. */
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (this.head === null){
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    if (this.length === 0) this.tail = this.head;
+
+    this.length +=1;
+  }
+
+  /** pop(): return & remove last item. */
+
+  pop() {
+    return this.removeAt(this.length - 1);
+  }
+
+  /** shift(): return & remove first item. */
+
+  shift() {
+    return this.removeAt(0)
+  }
+
+  /** getAt(idx): get val at idx. */
+
+  getAt(idx) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    return this._get(idx).val
+  }
+
+  /** setAt(idx, val): set val at idx to val */
+
+  setAt(idx, val) {
+    if (idx >= this.length || idx < 0){
+      throw new Error("invalid index");
+    }
+
+    let cur = this._get(idx);
+    cur.val = val;
+  }
+
+  /** insertAt(idx, val): add node w/val before idx. */
+
+  insertAt(idx, val) {
+    if (idx > this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    if (idx === 0) return this.unshift(val);
+    if (idx === this.length) return this.push(val);
+
+    // get one before it
+
+    let prev = this._get(idx - 1);
+
+    let newNode = new Node(val);
+    newNode.next = prev.next;
+    prev.next = newNode;
+
+    this.length += 1;
+  }
+
+  /** removeAt(idx): return & remove item at idx, */
+
+  removeAt(idx) {
+    if (idx >= this.length || idx < 0){
+      throw new Error("Invalid index.");
+    }
+
+    let val = prev.next.val;
+    prev.next = prev.next.next;
+    this.length -= 1;
+    return val;
+
+  }
+
+  /** average(): return an average of all values in the list */
+
+  average() {
+    if (this.length === 0) return 0;
+
+    let total =0;
+    while (current){
+      total += current.val;
+      current = current.next;
+
+    }
+    return toal / this.length;
+  }
+}
+
+module.exports = LinkedList;
